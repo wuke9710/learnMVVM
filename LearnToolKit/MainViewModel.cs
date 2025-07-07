edbe2d016eac4fde8014a7baa48cf4d3ac91c527
wuke <wu_ke@rf-met.com>
2025-07-07 17:31:19 +0800

    learn toolkit
    Signed-off-by: wuke <wu_ke@rf-met.com>

---
+++ LearnToolKit/MainViewModel.cs
@@ -0,0 +1,46 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Collections.ObjectModel;
+using System.Windows;
+using static LearnToolKit.MainViewModel;
+
+namespace LearnToolKit
+{
+    public partial class MainViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private string message = "Hello, .NET 8.0 + MVVM Toolkit!";
+
+        [ObservableProperty]
+        private Product? selectedProduct;
+
+        [RelayCommand]
+        private void ChangeMessage()
+        {
+            Message = "Message changed!";
+        }
+        public partial class Product : ObservableObject
+        {
+            [ObservableProperty]
+            private int id;
+            [ObservableProperty]
+            private string name = "123";
+        }
+        public ObservableCollection<Product> Products { get; } = new()
+        {
+            new Product { Id = 1, Name = "Apple" },
+            new Product { Id = 2, Name = "Banana" }
+        };
+
+        partial void OnSelectedProductChanged(Product? value)
+        {
+            if (value is null)
+                return;
+            if (!value.Name.EndsWith("Click"))
+            {
+                value.Name += "Click";
+            }
+        }
+
+    }
+}
